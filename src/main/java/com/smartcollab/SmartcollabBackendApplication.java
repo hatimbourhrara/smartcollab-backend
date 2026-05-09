5e478ce7baeb1803549b17333f8363c98dad971c
Hatim Bourhrara <hatim.bourhrara@stud.h-da.de>
2026-05-09 17:20:50 +0200

    chore: add postgres docker setup

--- src/main/java/com/smartcollab/SmartcollabBackendApplication.java
+++ src/main/java/com/smartcollab/SmartcollabBackendApplication.java
@@ -3,10 +3,7 @@ package com.smartcollab;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,
-        org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class SmartcollabBackendApplication {
 
 	public static void main(String[] args) {
