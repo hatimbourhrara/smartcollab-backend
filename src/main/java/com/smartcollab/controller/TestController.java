a560e217509fbc3cfa81f96308269aa36bdcdad0
Hatim Bourhrara <hatim.bourhrara@stud.h-da.de>
2026-05-10 17:17:09 +0200

    feat: add test api endpoint

---
+++ src/main/java/com/smartcollab/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.smartcollab.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public Map<String, String> test() {
+        return Map.of(
+                "message",
+                "SmartCollab backend is running"
+        );
+    }
+}
