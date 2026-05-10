99019e2cdb13d486eb8150784267a8ebcd70dcc1
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-05-10 19:17:37 +0200

    feat: add task repository

---
+++ src/main/java/com/smartcollab/task/repository/TaskRepository.java
@@ -0,0 +1,7 @@
+package com.smartcollab.task.repository;
+
+import com.smartcollab.task.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
