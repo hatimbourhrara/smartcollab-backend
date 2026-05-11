dd506f2d7eb6242fdf1c4cc9a62df6392bf17333
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-05-11 13:34:59 +0200

    feat: add create task endpoint

---
+++ src/main/java/com/smartcollab/task/TaskController.java
@@ -0,0 +1,23 @@
+package com.smartcollab.task;
+
+import com.smartcollab.task.dto.CreateTaskRequest;
+import com.smartcollab.task.model.Task;
+import com.smartcollab.task.service.TaskService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public Task createTask(@Valid @RequestBody CreateTaskRequest request) {
+        return taskService.createTask(request);
+    }
+}
