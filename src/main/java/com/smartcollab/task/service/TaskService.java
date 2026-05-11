b92e340505f2015f3576555ee8f0175412f05c5d
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-05-11 13:32:59 +0200

    feat: add task service

---
+++ src/main/java/com/smartcollab/task/service/TaskService.java
@@ -0,0 +1,26 @@
+package com.smartcollab.task.service;
+
+import com.smartcollab.task.dto.CreateTaskRequest;
+import com.smartcollab.task.model.Task;
+import com.smartcollab.task.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public Task createTask(CreateTaskRequest request) {
+
+        Task task = new Task();
+
+        task.setTitle(request.getTitle());
+        task.setDescription(request.getDescription());
+
+        return taskRepository.save(task);
+    }
+}
