2849f00840894abb48a752349f42f5b5141e4c7c
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-05-11 13:45:19 +0200

    feat: add get all tasks endpoint

--- src/main/java/com/smartcollab/task/TaskController.java
+++ src/main/java/com/smartcollab/task/TaskController.java
@@ -6,6 +6,8 @@ import com.smartcollab.task.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
@@ -19,5 +21,10 @@ public class TaskController {
     @PostMapping
     public Task createTask(@Valid @RequestBody CreateTaskRequest request) {
         return taskService.createTask(request);
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
     }
 }
