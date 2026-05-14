7c5bd1b89080d26f956183a53548c4ee3af0f780
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-05-14 13:14:29 +0200

    feat: add update task request dto

---
+++ src/main/java/com/smartcollab/task/dto/UpdateTaskRequest.java
@@ -0,0 +1,20 @@
+package com.smartcollab.task.dto;
+
+import jakarta.validation.constraints.Size;
+
+public class UpdateTaskRequest {
+
+    @Size(min = 3, max = 100)
+    private String title;
+
+    @Size(max = 500)
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
