30ab3bf80f2a97a64dd050773ef867c315f48c9a
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-05-19 00:35:56 +0200

    feat: add find tasks by status repository method

--- src/main/java/com/smartcollab/task/repository/TaskRepository.java
+++ src/main/java/com/smartcollab/task/repository/TaskRepository.java
@@ -1,7 +1,12 @@
 package com.smartcollab.task.repository;
 
 import com.smartcollab.task.model.Task;
+import com.smartcollab.task.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByStatus(TaskStatus status);
 }
