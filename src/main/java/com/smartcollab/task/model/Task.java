1759371eec05d3fd0f65935ff1bd287c9739e32d
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-05-27 00:20:54 +0200

    feat: add task creator field

--- src/main/java/com/smartcollab/task/model/Task.java
+++ src/main/java/com/smartcollab/task/model/Task.java
@@ -1,15 +1,11 @@
 package com.smartcollab.task.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "tasks")
-@Getter
-@Setter
 public class Task {
 
     @Id
@@ -21,16 +17,49 @@ public class Task {
     private String description;
 
     @Enumerated(EnumType.STRING)
-    private TaskStatus status;
+    private TaskStatus status = TaskStatus.TODO;
 
-    private LocalDateTime createdAt;
+    private String createdBy;
 
-    @PrePersist
-    public void prePersist() {
-        createdAt = LocalDateTime.now();
+    private LocalDateTime createdAt = LocalDateTime.now();
 
-        if (status == null) {
-            status = TaskStatus.TODO;
-        }
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
     }
 }
