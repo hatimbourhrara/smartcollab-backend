ecd00aad18f29fc33d623389dadbfb70c34f6462
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-06-04 00:32:35 +0200

    feat: add project id to create task request

--- src/main/java/com/smartcollab/task/dto/CreateTaskRequest.java
+++ src/main/java/com/smartcollab/task/dto/CreateTaskRequest.java
@@ -1,6 +1,7 @@
 package com.smartcollab.task.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class CreateTaskRequest {
@@ -12,11 +13,18 @@ public class CreateTaskRequest {
     @Size(max = 500)
     private String description;
 
+    @NotNull(message = "Project id is required")
+    private Long projectId;
+
     public String getTitle() {
         return title;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public Long getProjectId() {
+        return projectId;
     }
 }
