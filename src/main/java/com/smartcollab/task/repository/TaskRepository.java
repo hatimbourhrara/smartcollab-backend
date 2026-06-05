51488a8143d45359cd4112ca08fc40cf8d77faa2
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-06-06 01:56:59 +0200

    feat: add find tasks by project repository method

--- src/main/java/com/smartcollab/task/repository/TaskRepository.java
+++ src/main/java/com/smartcollab/task/repository/TaskRepository.java
@@ -1,5 +1,6 @@
 package com.smartcollab.task.repository;
 
+import com.smartcollab.project.model.Project;
 import com.smartcollab.task.model.Task;
 import com.smartcollab.task.model.TaskStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ public interface TaskRepository extends JpaRepository<Task, Long> {
     List<Task> findByStatus(TaskStatus status);
 
     List<Task> findByCreatedBy(String createdBy);
+
+    List<Task> findByProject(Project project);
 }
