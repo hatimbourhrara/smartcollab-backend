168f3ebf4ee37541c9df1f791d58281062274b86
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-06-06 02:00:16 +0200

    feat: add project ownership validation

---
+++ src/main/java/com/smartcollab/project/ProjectService.java
@@ -0,0 +1,61 @@
+package com.smartcollab.project.service;
+
+import com.smartcollab.common.exception.ResourceNotFoundException;
+import com.smartcollab.project.model.Project;
+import com.smartcollab.project.repository.ProjectRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectService {
+
+    private final ProjectRepository projectRepository;
+
+    public ProjectService(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    public Project createProject(
+            String name,
+            String description,
+            String userEmail
+    ) {
+
+        Project project = new Project();
+
+        project.setName(name);
+        project.setDescription(description);
+        project.setCreatedBy(userEmail);
+
+        return projectRepository.save(project);
+    }
+
+    public List<Project> getMyProjects(String userEmail) {
+        return projectRepository.findByCreatedBy(userEmail);
+    }
+
+    public Project getProjectById(Long id) {
+
+        return projectRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Project not found")
+                );
+    }
+
+    public Project getUserProjectById(
+            Long id,
+            String userEmail
+    ) {
+
+        Project project = getProjectById(id);
+
+        if (!project.getCreatedBy().equals(userEmail)) {
+            throw new RuntimeException(
+                    "You are not allowed to access this project"
+            );
+        }
+
+        return project;
+    }
+}
