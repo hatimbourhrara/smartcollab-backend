2a576c4184df52bcf41ce6b42f123c9a1985eb6f
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-06-09 00:13:14 +0200

    feat: add project member service

---
+++ src/main/java/com/smartcollab/project/service/ProjectMemberService.java
@@ -0,0 +1,55 @@
+package com.smartcollab.project.service;
+
+import com.smartcollab.project.model.Project;
+import com.smartcollab.project.model.ProjectMember;
+import com.smartcollab.project.repository.ProjectMemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProjectMemberService {
+
+    private final ProjectMemberRepository projectMemberRepository;
+    private final ProjectService projectService;
+
+    public ProjectMemberService(
+            ProjectMemberRepository projectMemberRepository,
+            ProjectService projectService
+    ) {
+        this.projectMemberRepository = projectMemberRepository;
+        this.projectService = projectService;
+    }
+
+    public ProjectMember addMember(
+            Long projectId,
+            String memberEmail,
+            String ownerEmail
+    ) {
+
+        Project project = projectService.getUserProjectById(
+                projectId,
+                ownerEmail
+        );
+
+        ProjectMember member = new ProjectMember();
+
+        member.setUserEmail(memberEmail);
+        member.setProject(project);
+
+        return projectMemberRepository.save(member);
+    }
+
+    public List<ProjectMember> getProjectMembers(
+            Long projectId,
+            String ownerEmail
+    ) {
+
+        Project project = projectService.getUserProjectById(
+                projectId,
+                ownerEmail
+        );
+
+        return projectMemberRepository.findByProject(project);
+    }
+}
