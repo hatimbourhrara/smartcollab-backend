be8676a7dce08efba7638610610e301521d41d9e
Hatim Bourhrara <mr.hatimbourhrara@gmail.com>
2026-06-28 13:50:40 +0200

    feat: log activity for comments

--- src/main/java/com/smartcollab/comment/service/CommentService.java
+++ src/main/java/com/smartcollab/comment/service/CommentService.java
@@ -1,5 +1,6 @@
 package com.smartcollab.comment.service;
 
+import com.smartcollab.activity.service.ActivityLogService;
 import com.smartcollab.comment.model.Comment;
 import com.smartcollab.comment.repository.CommentRepository;
 import com.smartcollab.task.model.Task;
@@ -13,13 +14,16 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
     private final TaskService taskService;
+    private final ActivityLogService activityLogService;
 
     public CommentService(
             CommentRepository commentRepository,
-            TaskService taskService
+            TaskService taskService,
+            ActivityLogService activityLogService
     ) {
         this.commentRepository = commentRepository;
         this.taskService = taskService;
+        this.activityLogService = activityLogService;
     }
 
     public Comment createComment(
@@ -36,7 +40,14 @@ public class CommentService {
         comment.setCreatedBy(userEmail);
         comment.setTask(task);
 
-        return commentRepository.save(comment);
+        Comment savedComment = commentRepository.save(comment);
+
+        activityLogService.logActivity(
+                "Added comment to task #" + taskId,
+                userEmail
+        );
+
+        return savedComment;
     }
 
     public List<Comment> getTaskComments(Long taskId) {
@@ -45,17 +56,23 @@ public class CommentService {
 
         return commentRepository.findByTask(task);
     }
+
     public void deleteComment(
-        Long commentId,
-        String userEmail
-) {
+            Long commentId,
+            String userEmail
+    ) {
 
-    Comment comment = commentRepository
-            .findByIdAndCreatedBy(commentId, userEmail)
-            .orElseThrow(() ->
-                    new RuntimeException("Comment not found")
-            );
+        Comment comment = commentRepository
+                .findByIdAndCreatedBy(commentId, userEmail)
+                .orElseThrow(() ->
+                        new RuntimeException("Comment not found")
+                );
 
-    commentRepository.delete(comment);
-}
+        commentRepository.delete(comment);
+
+        activityLogService.logActivity(
+                "Deleted comment #" + commentId,
+                userEmail
+        );
+    }
 }
